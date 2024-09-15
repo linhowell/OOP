e749b811cbb292327dade264f643c7a68f61a6be
linhowell <48195819+linhowell@users.noreply.github.com>
2024-09-15 21:22:41 +0800

    Update Rectangle.java

--- posd2024f_ta-assignment1_template_code/src/main/java/org/ntut/posd2024f/shapes/Rectangle.java
+++ posd2024f_ta-assignment1_template_code/src/main/java/org/ntut/posd2024f/shapes/Rectangle.java
@@ -3,14 +3,14 @@ package org.ntut.posd2024f.shapes;
 public class Rectangle implements Shape {
     double h,w;
     public Rectangle(double length, double width) throws Exception {
-        if(check(length, width)){
-            throw new ArithmeticException("It's not a Rectangle!");
+        if(!check(length, width)){
+            throw new ArithmeticException("It's not a rectangle!");
         }
         h = length;
         w = width;
     }
     public boolean check(double length, double width){
-        if(length < 0 || width <0){
+        if(length < 0 || width < 0){
             return false;
         }
         return true;
